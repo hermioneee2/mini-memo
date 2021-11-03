9d9062c168dcba86ab231690ab406c820dfbc83a
Yusung Sim <yusungsim@kaist.ac.kr>
2021-11-03 23:01:03 +0900

    Added antd.css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Header from "./components/Header";
 import PostIt from "./components/PostIt";
 import List from "./components/List";
 import AddMemo from "./components/AddMemo";
+import "antd/dist/antd.css";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <AddMemo />
       <PostIt />
+      <AddMemo />
     </div>
   );
 };
