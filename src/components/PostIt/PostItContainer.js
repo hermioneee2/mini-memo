9260fb07af8be38abac726a50191b6412022547a
Yusung Sim <yusungsim@kaist.ac.kr>
2021-11-03 23:02:35 +0900

    PoC for PostIt

--- src/components/PostIt/PostItContainer.js
+++ src/components/PostIt/PostItContainer.js
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
 import PostItPresenter from "./PostItPresenter";
 
 const PostItContainer = () => {
-  return <PostItPresenter />;
+  const [title, setTitle] = useState("Some fancy title");
+  const [content, setContent] = useState("wowowow");
+
+  const onChangeTitle = (e) => {
+    setTitle(e)
+  }
+
+  const onChangeContent = (e) => {
+    setContent(e)
+  }
+
+  return <PostItPresenter title={title} content={content}/>;
 };
 
 export default PostItContainer;
--- src/components/PostIt/PostItPresenter.js
+++ src/components/PostIt/PostItPresenter.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Card } from "antd";
 
-const PostItPresenter = () => {
-  return <div>PostIt</div>;
+import styled from "styled-components";
+
+const PostItPresenter = ({ title, content }) => {
+  const style = {
+    width: "400px",
+    margin: "10px",
+  };
+
+  return (
+    <div style={style}>
+      <Card title={title} style={style}>
+        {content}
+      </Card>
+    </div>
+  );
 };
 
 export default PostItPresenter;
