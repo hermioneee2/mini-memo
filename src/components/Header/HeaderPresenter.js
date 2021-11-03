40b12690a363ccfcb933defe936549f6aa2847db
hermioneee2 <hermioneee2@gmail.com>
2021-11-03 11:04:00 +0900

    Top buttons added

--- src/components/Header/HeaderPresenter.js
+++ src/components/Header/HeaderPresenter.js
@@ -1,4 +1,11 @@
-import { Affix } from "antd";
+
+import {
+    BarsOutlined,
+    AppstoreOutlined,
+    DeleteOutlined,
+    SettingOutlined,
+} from "@ant-design/icons";
+import { Affix, Button, Dropdown } from "antd";
 import React from "react";
 import styled from "styled-components";
 
@@ -17,7 +24,13 @@ const HeaderPresenter = () => {
             >
                 Mini Memo
             </span>
+            <HeaderButtonWrapper>
+                <BarsOutlined style={{ fontSize: 28, color: "#F0BF39", cursor: "pointer" }} />
+                <DeleteOutlined style={{ fontSize: 28, color: "#F0BF39", cursor: "pointer" }} />
+                <SettingOutlined style={{ fontSize: 28, color: "#F0BF39", cursor: "pointer" }} />
+            </HeaderButtonWrapper>
         </Header>
+        <HeaderBottomOutline></HeaderBottomOutline>
     </Affix>
     );
 };
@@ -30,6 +43,22 @@ const Header = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+`;
+
+const HeaderButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-right: 40px;
+  width: 130px;
+  justify-content: space-between;
+`;
+
+const HeaderBottomOutline = styled.div`
+  background-color: #d9d9d9;
+  width: 100%;
+  height: 2px;
+  display: flex;
 `;
 
 export default HeaderPresenter;
