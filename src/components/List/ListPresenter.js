a7a4b5834ac5bfa28ec6cb2b9a14ebcfdaf892b1
Yusung Sim <yusungsim@kaist.ac.kr>
2021-11-04 00:24:13 +0900

    Revert verbose list

--- src/components/List/ListPresenter.js
+++ src/components/List/ListPresenter.js
@@ -1,28 +1,8 @@
 import React from "react";
 import ListItem from "../ListItem";
-import { List, Divider } from "antd";
+import { List } from "antd";
 
 const data = [
-  { title: "title1", content: "content1" },
-  { title: "title2", content: "content2" },
-  { title: "title3", content: "content3" },
-  { title: "title4", content: "content4" },
-  { title: "title1", content: "content1" },
-  { title: "title2", content: "content2" },
-  { title: "title3", content: "content3" },
-  { title: "title4", content: "content4" },
-  { title: "title1", content: "content1" },
-  { title: "title2", content: "content2" },
-  { title: "title3", content: "content3" },
-  { title: "title4", content: "content4" },
-  { title: "title1", content: "content1" },
-  { title: "title2", content: "content2" },
-  { title: "title3", content: "content3" },
-  { title: "title4", content: "content4" },
-  { title: "title1", content: "content1" },
-  { title: "title2", content: "content2" },
-  { title: "title3", content: "content3" },
-  { title: "title4", content: "content4" },
   { title: "title1", content: "content1" },
   { title: "title2", content: "content2" },
   { title: "title3", content: "content3" },
@@ -31,14 +11,14 @@ const data = [
 
 const ListPresenter = () => {
   return (
-      <List
-        dataSource={data}
-        renderItem={(item) => (
-          <List.Item>
-            <ListItem title={item.title} content={item.content} />
-          </List.Item>
-        )}
-      />
+    <List
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item>
+          <ListItem title={item.title} content={item.content} />
+        </List.Item>
+      )}
+    />
   );
 };
 
