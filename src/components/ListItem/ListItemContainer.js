364d5907836e1e547e433f0677e9c50cdbe21157
YunKim-kaist <acroyun@kaist.ac.kr>
2021-11-12 14:38:04 +0900

    structure modified

--- src/components/ListItem/ListItemContainer.js
+++ src/components/ListItem/ListItemContainer.js
@@ -2,19 +2,14 @@ import ListItemPresenter from "./ListItemPresenter";
 import {useState} from "react";
 
 const ListItemContainer = ({title, content, uid, click_num, getData, delItems, setTrue, setId}) => {
-  // const [checkedItems, setCheckedItems] = useState(new Set());
   const [bChecked, setChecked] = useState(false);
 
   const checkedItemHandler = (id, isChecked) => {
     if (isChecked) {
-      // checkedItems.add(id);
       delItems.add(id);
-      // setCheckedItems(checkedItems);
     } 
     else if (!isChecked) {
-      // checkedItems.delete(id);
       delItems.delete(id);
-      // setCheckedItems(checkedItems);
     }
     getData(delItems);
   };
