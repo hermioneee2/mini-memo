caafe325d304bafa214cd1ed241b333218473919
hermioneee2 <hermioneee2@gmail.com>
2021-11-22 02:11:33 +0900

    clean up

--- server/server.js
+++ server/server.js
@@ -8,14 +8,8 @@ const port = process.env.PORT || 3031;
 
 app.use(cors());
 
-// app.set("port", port);
 app.listen(port, () => {
   console.log(`express is running on ${port}`);
 });
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "jade");
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
