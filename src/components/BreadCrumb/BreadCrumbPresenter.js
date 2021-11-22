b5d66fd6119c89e991f2ab644b5150533adad7e7
qwqw3535 <78776430+qwqw3535@users.noreply.github.com>
2021-11-22 18:33:11 +0900

    folderUI-still working

--- src/components/BreadCrumb/BreadCrumbPresenter.js
+++ src/components/BreadCrumb/BreadCrumbPresenter.js
@@ -1,14 +1,15 @@
 import React, {useState} from "react"
 import {Breadcrumb} from "antd"
+import BreadcrumbItem from "antd/lib/breadcrumb/BreadcrumbItem";
 
 
 const BreadCrumbPresenter = (cwd) => {
     //console.log(cwd.cwd.cwd)
     let hierarchy =[];
+    let folder_title = ""
     const real_cwd = cwd.cwd.cwd
-    let parent = real_cwd.parent()
     //console.log(parent)
-    console.log(real_cwd)
+    //console.log(real_cwd)
     //hierarchy.push("root")
     const adding_hierarchy = (real_cwd) => {
         if(real_cwd.name !='/'){
@@ -22,12 +23,51 @@ const BreadCrumbPresenter = (cwd) => {
     }
     adding_hierarchy(real_cwd)
     
+    if (real_cwd.name ==='/'){
+        folder_title = "home"
+    }
+    else{
+        folder_title = real_cwd.name
+    }
     //console.log(hierarchy)
     const hierarchy_render = hierarchy.map((pt) => 
     (<Breadcrumb.Item>{pt}</Breadcrumb.Item>))
     return (
-        hierarchy_render
+        <div style = {BreadcrumbBox_style}>
+            <Breadcrumb style = {Breadcrumb_style}>
+                {hierarchy_render}
+            </Breadcrumb>
+            <div style = {{padding: "14px 0px 6px", fontSize:18}}>{folder_title}</div>
+        </div>
     )
+}
+
+
+const BreadcrumbBox_style = {
+
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    padding: "16px 24px",
+    margin: "auto",
+    position: "relative",
+    width: "80%",
+    height: "102px",
+    
+    top:"30px",
+    bottom:"30px",
+    backgroundColor: "#FAFAFA",
+    borderRadius: "10px" 
+}
+const Breadcrumb_style = {
+
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    padding:"0px",
+    fontFamily:"Roboto",
+    fontStyle:"normal",
+    fontWeight:"normal"
 }
 
 export default BreadCrumbPresenter;
