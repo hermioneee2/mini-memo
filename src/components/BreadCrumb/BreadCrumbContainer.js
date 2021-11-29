86ef1d8cdc9191ee6fdeaef5390ac1d8bcc1f718
Yusung Sim <yusungsim@kaist.ac.kr>
2021-11-29 16:49:06 +0900

    Changed library to npm, @systemop/localstorage-fs

--- src/components/BreadCrumb/BreadCrumbContainer.js
+++ src/components/BreadCrumb/BreadCrumbContainer.js
@@ -1,6 +1,6 @@
 import BreadCrumbPresenter from "./BreadCrumbPresenter"
 import React from "react"
-import {get_root_pointer} from "../../memo-storage/localstorage-fs/fs-pointer"
+import {get_root_pointer} from "@systemop/localstorage-fs"
 const BreadCrumbContainer = (cwd) => {
     const root_pointer =get_root_pointer() ;
     
